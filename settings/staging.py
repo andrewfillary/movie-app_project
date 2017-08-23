75dd6a4740fc39c4a2ab21a01f3e1ad16653a7a4
andrewfillary <afillary@hotmail.com>
2017-08-23 23:19:58 +0100

    replaces database connections

--- settings/staging.py
+++ settings/staging.py
@@ -4,17 +4,15 @@ import dj_database_url
 DEBUG = False
 
 DATABASES = {
-    'default': dj_database_url.config('mysql://b43e9fb686d9c3:979c6f2b@eu-cdbr-west-01.cleardb.com/heroku_34a0eee9a408153'),
-    'ENGINE': 'django.db.backends.sqlite3'
+    'default': dj_database_url.config('mysql://b43e9fb686d9c3:979c6f2b@eu-cdbr-west-01.cleardb.com/heroku_34a0eee9a408153')
 }
 
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Stripe environment variables
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE', '<your STRIPE_PUBLISHABLE key>')
